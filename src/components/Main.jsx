9a3cc82535e151e0cc56ac0781db5e0fe41f41f8
zntb <zenetibi@yahoo.com>
2024-09-26 10:43:08 +0300

    Add finished exercise 10.4

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,18 @@
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import AppBar from './AppBar';
 import RepositoryList from './RepositoryList';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#e1e4e8',
+  },
+});
 
 const Main = () => {
   return (
-    <View>
+    <View style={styles.container}>
+      <AppBar />
       <RepositoryList />
     </View>
   );
