321204743a11a9c52146143bd8dec162e88ae2bd
zntb <zntbdev@proton.me>
2024-09-26 09:55:01 +0300

    Add finished exercise 10.3

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import { View } from 'react-native';
+import RepositoryList from './RepositoryList';
+
+const Main = () => {
+  return (
+    <View>
+      <RepositoryList />
+    </View>
+  );
+};
+
+export default Main;
