e71dc113e4c13da4b465907e42a5fd1fd048369c
zntb <zntbdev@proton.me>
2024-09-27 08:36:36 +0300

    Add finished exercise 10.7

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,6 +1,6 @@
-import { View, Text, StyleSheet } from 'react-native';
-import { Link } from 'react-router-native';
+import { View, Text, StyleSheet, Pressable, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
+import { Link } from 'react-router-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -8,6 +8,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#24292e',
     paddingBottom: 10,
     paddingLeft: 10,
+  },
+  scrollContainer: {
     flexDirection: 'row',
   },
   tab: {
@@ -20,12 +22,32 @@ const styles = StyleSheet.create({
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <Link to='/'>
-        <Text style={styles.tab}>Repositories</Text>
-      </Link>
-      <Link to='/signin'>
-        <Text style={styles.tab}>Sign in</Text>
-      </Link>
+      <ScrollView horizontal contentContainerStyle={styles.scrollContainer}>
+        <Link to='/'>
+          <Text style={styles.tab}>Repositories</Text>
+        </Link>
+        <Link to='/signin'>
+          <Text style={styles.tab}>Sign in</Text>
+        </Link>
+        <Link to='/tab3'>
+          <Text style={styles.tab}>Tab 3</Text>
+        </Link>
+        <Link to='/tab4'>
+          <Text style={styles.tab}>Tab 4</Text>
+        </Link>
+        <Link to='/tab5'>
+          <Text style={styles.tab}>Tab 5</Text>
+        </Link>
+        <Link to='/tab6'>
+          <Text style={styles.tab}>Tab 6</Text>
+        </Link>
+        <Link to='/tab7'>
+          <Text style={styles.tab}>Tab 7</Text>
+        </Link>
+        <Link to='/tab8'>
+          <Text style={styles.tab}>Tab 8</Text>
+        </Link>
+      </ScrollView>
     </View>
   );
 };
