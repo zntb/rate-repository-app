ad8a1d569278a39069b52796c83cfc185aa019bb
zntb <zenetibi@yahoo.com>
2024-09-27 08:20:53 +0300

    Add finished exercise 10.6

--- App.js
+++ App.js
@@ -1,7 +1,16 @@
 import Main from './src/components/Main';
+import { NativeRouter } from 'react-router-native';
+import { StatusBar } from 'expo-status-bar';
 
 const App = () => {
-  return <Main />;
+  return (
+    <>
+      <NativeRouter>
+        <Main />
+      </NativeRouter>
+      <StatusBar style='auto' />
+    </>
+  );
 };
 
 export default App;
