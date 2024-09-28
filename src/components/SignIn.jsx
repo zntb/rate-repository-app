aa259935e9d516564495dba5011c19472f4342f8
zntb <zenetibi@yahoo.com>
2024-09-28 12:15:21 +0300

    Add finished exercise 10.13

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,9 +1,9 @@
 import { StyleSheet, View } from 'react-native';
 import { Formik } from 'formik';
 import * as yup from 'yup';
-
 import Button from './Button';
 import FormikTextInput from './FormikTextInput';
+import useSignIn from '../hooks/useSignIn';
 
 const styles = StyleSheet.create({
   container: {
@@ -25,27 +25,30 @@ const validationSchema = yup.object().shape({
   password: yup.string().required('Password is required'),
 });
 
-const SignInForm = ({ onSubmit }) => {
-  return (
-    <View style={styles.container}>
-      <View style={styles.fieldContainer}>
-        <FormikTextInput name='username' placeholder='Username' />
-      </View>
-      <View style={styles.fieldContainer}>
-        <FormikTextInput
-          name='password'
-          placeholder='Password'
-          secureTextEntry
-        />
-      </View>
-      <Button onPress={onSubmit}>Sign in</Button>
+const SignInForm = ({ onSubmit }) => (
+  <View style={styles.container}>
+    <View style={styles.fieldContainer}>
+      <FormikTextInput name='username' placeholder='Username' />
     </View>
-  );
-};
+    <View style={styles.fieldContainer}>
+      <FormikTextInput name='password' placeholder='Password' secureTextEntry />
+    </View>
+    <Button onPress={onSubmit}>Sign in</Button>
+  </View>
+);
 
 const SignIn = () => {
-  const onSubmit = values => {
-    console.log(values);
+  const [signIn] = useSignIn();
+
+  const onSubmit = async values => {
+    const { username, password } = values;
+
+    try {
+      const response = await signIn({ username, password });
+      console.log('Signed in successfully:', response);
+    } catch (e) {
+      console.error('Sign-in failed:', e);
+    }
   };
 
   return (
