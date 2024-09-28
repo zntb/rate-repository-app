e06cac1e2a9f2c272872b49239d4f2acf2674093
zntb <zntbdev@proton.me>
2024-09-28 11:20:24 +0300

    Add finished exercise 10.12

--- App.js
+++ App.js
@@ -3,10 +3,13 @@ import { NativeRouter } from 'react-router-native';
 import { StatusBar } from 'expo-status-bar';
 import { ApolloProvider } from '@apollo/client';
 import createApolloClient from './src/utils/apolloClient';
+import Constants from 'expo-constants';
 
 const apolloClient = createApolloClient();
 
 const App = () => {
+  console.log(Constants.expoConfig);
+
   return (
     <>
       <NativeRouter>
