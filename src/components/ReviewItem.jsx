eb25abc512c0d04815da73335c32bfce9fa79567
zntb <zenetibi@yahoo.com>
2024-09-30 14:50:56 +0300

    Add finished exercise 10.26

--- src/components/ReviewItem.jsx
+++ src/components/ReviewItem.jsx
@@ -1,5 +1,9 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Alert } from 'react-native';
 import { format } from 'date-fns';
+import { useNavigate } from 'react-router-native';
+import { useMutation } from '@apollo/client';
+import { DELETE_REVIEW } from '../graphql/mutations';
+import Button from './Button';
 
 const styles = StyleSheet.create({
   container: {
@@ -22,11 +26,56 @@ const styles = StyleSheet.create({
   reviewContent: { flex: 1 },
   username: { fontWeight: 'bold', marginBottom: 5 },
   date: { color: '#666', marginBottom: 5 },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10,
+  },
 });
 
-const ReviewItem = ({ review, showRepositoryName = false }) => {
-  const { text, rating, createdAt, user, repository } = review;
+const ReviewItem = ({
+  review,
+  showRepositoryName = false,
+  refetch,
+  showActions = true,
+}) => {
+  const { text, rating, createdAt, user, repository, id } = review;
   const formattedDate = format(new Date(createdAt), 'dd.MM.yyyy');
+  const navigate = useNavigate();
+  const [deleteReview] = useMutation(DELETE_REVIEW, {
+    onCompleted: () => {
+      refetch();
+    },
+  });
+
+  const handleDelete = () => {
+    Alert.alert(
+      'Delete Review',
+      'Are you sure you want to delete this review?',
+      [
+        {
+          text: 'Cancel',
+          style: 'cancel',
+        },
+        {
+          text: 'Delete',
+          style: 'destructive',
+          onPress: async () => {
+            try {
+              await deleteReview({ variables: { id } });
+            } catch (e) {
+              console.error(e);
+            }
+          },
+        },
+      ],
+      { cancelable: true },
+    );
+  };
+
+  const handleViewRepository = () => {
+    navigate(`/repository/${repository.id}`);
+  };
 
   return (
     <View style={styles.container}>
@@ -41,6 +90,16 @@ const ReviewItem = ({ review, showRepositoryName = false }) => {
         )}
         <Text style={styles.date}>{formattedDate}</Text>
         <Text>{text}</Text>
+
+        {showActions && (
+          <View style={styles.buttonContainer}>
+            <Button onPress={handleViewRepository}>View Repository</Button>
+            <Button type='alertButton' onPress={handleDelete}>
+              {' '}
+              Delete Review
+            </Button>
+          </View>
+        )}
       </View>
     </View>
   );
--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -31,3 +31,9 @@ export const CREATE_USER = gql`
     }
   }
 `;
+
+export const DELETE_REVIEW = gql`
+  mutation deleteReview($id: ID!) {
+    deleteReview(id: $id)
+  }
+`;
